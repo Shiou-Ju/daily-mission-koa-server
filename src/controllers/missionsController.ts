05c4d3b68a0d9080fb4bb59fa9a812b6d0a379a1
Shiou-Ju <75402531+Shiou-Ju@users.noreply.github.com>
2022-06-25 18:09:40 +0800

    feat: all controller prototype set

--- src/controllers/missionsController.ts
+++ src/controllers/missionsController.ts
@@ -5,8 +5,9 @@ export const getSingleMission = async (
   ctx: Router.RouterContext,
   next: Koa.Next
 ) => {
-  const a = ctx.params.id;
-  ctx.body = { data: 'test', a };
+  // TODO:
+  const id = ctx.params.id;
+  ctx.body = { data: 'test', id };
   await next();
 };
 
@@ -14,7 +15,17 @@ export const updateSingleMission = async (
   ctx: Router.RouterContext,
   next: Koa.Next
 ) => {
+  // TODO:
   const requestBody = ctx.request.body;
   ctx.body = { data: 'received', requestBody };
+  await next();
+};
+
+export const getAllMissions = async (
+  ctx: Router.RouterContext,
+  next: Koa.Next
+) => {
+  // TODO:
+  ctx.body = { data: 'all missions' };
   await next();
 };
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,12 +2,13 @@ import Router from 'koa-router';
 import {
   getSingleMission,
   updateSingleMission,
+  getAllMissions,
 } from '../controllers/missionsController';
 
 const router = new Router();
 
+router.get('/missions', getAllMissions);
 router.get('/missions/:id', getSingleMission);
-
 router.post('/missions/:id', updateSingleMission);
 
 export { router };
